d6e7764493a5a8901651058efabe7286e6c33420
snehallabade <snehallabade13@gmail.com>
2025-06-12 00:17:09 +0530

    Render 4

--- server/utils/paths.ts
+++ server/utils/paths.ts
@@ -4,15 +4,20 @@ import { fileURLToPath } from 'url';
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const rootDir = path.resolve(__dirname, '../../');
 
 export const resolveServerPath = (...parts: string[]) => {
-    return path.resolve(__dirname, ...parts);
+    return path.resolve(rootDir, 'server', ...parts);
 };
 
 export const resolveClientPath = (...parts: string[]) => {
-    return path.resolve(__dirname, '../client', ...parts);
+    return path.resolve(rootDir, 'client', ...parts);
 };
 
 export const resolvePublicPath = (...parts: string[]) => {
-    return path.resolve(__dirname, '../dist/public', ...parts);
+    return path.resolve(rootDir, 'dist/public', ...parts);
+};
+
+export const resolveRootPath = (...parts: string[]) => {
+    return path.resolve(rootDir, ...parts);
 };
